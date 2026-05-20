6729b8418482f8ca48653efea29daf258ca524f1
Jackson Cypriano Cobra <jackson.cobra@globalhitss.com.br>
2026-05-20 11:07:13 -0300

    feat: adicionar validações no modelo de usuário e permissões no admin de tenant

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+
 from apps.tenants.models import Tenant
+
 
 class User(AbstractUser):
     tenant = models.ForeignKey(Tenant, null=True, blank=True, on_delete=models.CASCADE)
     is_tenant_admin = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser and self.tenant_id:
+            raise ValidationError("Superuser não pode ter tenant")
+
+        if not self.is_superuser and not self.tenant_id:
+            raise ValidationError("User comum precisa de tenant")
+
+        return super().save(*args, **kwargs)
--- apps/core/admin.py
+++ apps/core/admin.py
@@ -23,3 +23,18 @@ class TenantModelAdmin(ModelAdmin):
         if tenant and db_field.name in ('tenant', 'category', 'product'):
             field.queryset = field.queryset.filter(tenant=tenant)
         return field
+
+    def has_module_permission(self, request):
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
